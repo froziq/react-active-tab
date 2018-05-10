40cddb0373367059d84e460d734245583b7c8f60
Yuriy Markov <markov.yuriy94@gmail.com>
2018-05-10 13:59:56 +0300

    add close icon

---
+++ src/icons/Close.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Close() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+      <path fill="currentColor" fillRule="evenodd" d="M7.11 9.17L.65 2.707A1.337 1.337 0 1 1 2.54.818L9 7.278l6.46-6.46a1.337 1.337 0 1 1 1.89 1.89L10.89 9.17l6.123 6.123a1.337 1.337 0 1 1-1.89 1.89L9 11.059l-6.122 6.123a1.337 1.337 0 1 1-1.89-1.89L7.11 9.169z"/>
+    </svg>
+  );
+}
+
+export default Close;
