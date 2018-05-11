eba18f7d99cb097f171ee583a9d8452948719909
Yuriy Markov <markov.yuriy94@gmail.com>
2018-05-11 12:32:06 +0300

    start CRA

--- es/Wrapper.js
+++
@@ -1,9 +0,0 @@
-var _templateObject = _taggedTemplateLiteralLoose(['\n  position: relative;\n'], ['\n  position: relative;\n']);
-
-function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
-
-import styled from 'styled-components';
-
-var Wrapper = styled.div(_templateObject);
-
-export default Wrapper;
